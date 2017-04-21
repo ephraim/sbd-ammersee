6d4514319e2055e2861616a496b1dc3006aafc51
Thilo Cestonaro <thilo@cestona.ro>
2017-04-21 14:29:28 +0200

    added commandline options help, version and port

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <QApplication>
 #include <QQmlContext>
 #include <QQmlApplicationEngine>
+#include <QCommandLineParser>
 #include <QtQml>
 #include "twn4.sys.h"
 #include "qsimpleproto.h"
@@ -8,14 +9,28 @@
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
+	QCoreApplication::setApplicationName("ammersee");
+    QCoreApplication::setApplicationVersion("1.0");
 
-	QSimpleProtocolClient spc;
+	QString port("/dev/ttyACM0");
+	QCommandLineParser parser;
+	QCommandLineOption portOption(QStringList() << "p" << "port", "RFID-Reader serial port", "port");
+
+	parser.addHelpOption();
+	parser.addVersionOption();
+	parser.setApplicationDescription("SB Delphin 03 Augsburg - Ammerseeschwimmen");
+	parser.addOption(portOption);
+	parser.process(app);
+
+	if (parser.isSet(portOption))
+		port = parser.value(portOption);
+
+	QSimpleProtocolClient spc(port);
 	QQmlApplicationEngine engine;
 	QQmlContext *cntxt = engine.rootContext();
 
 	spc.setTagTypes(TAGMASK(LFTAG_EM4102), TAGMASK(HFTAG_MIFARE));
 	cntxt->setContextProperty("spc", &spc);
 	engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-
 	return app.exec();
 }
